de9021cefb9f41b58ef012db5dbaa9753af5c150
AYAM M SHUKLA <ayamshukla@ayams-MacBook-Air.local>
2026-07-06 21:09:41 +0530

    Update DB scripts to support environment variable URL fallback

--- fixOwner.js
+++ fixOwner.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Listing = require('./models/listing');
 const User = require('./models/user');
  
-mongoose.connect('mongodb://127.0.0.1:27017/wanderlust').then(async () => {
+mongoose.connect(process.env.ATLASDB_URL || 'mongodb://127.0.0.1:27017/wanderlust').then(async () => {
     // Assign all ownerless listings to first user
     const user = await User.findOne({});
     await Listing.updateMany({ owner: null }, { owner: user._id });
